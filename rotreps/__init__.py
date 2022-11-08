ec15df907b99fb406f4bc03ce66aa73ab3da3e13
Jonathan Van Schenck <jvschenck@novadynamics.com>
2022-11-08 08:28:30 -0800

    Add versioning

---
+++ rotreps/__init__.py
@@ -0,0 +1,8 @@
+"""Rotreps - A euclidean transformation representation library
+"""
+
+from . import transformations
+from . import symbolic_transformations as symbolic
+
+__author__ = "Jonathan D. B. Van Schenck"
+__version__ = "0.1.0"
