Fixed quaternion weirdness

test7.py
@@ -44,7 +44,7 @@ def dead_reckon_step_errors( dl, dr, dl_scale=0.005):
     #  [ var_dl, cov_dldr, var_dr, var_b, var_c, var_d ]
     return [ ddl*ddl + 1e-8, rho*ddl*ddr, ddr*ddr + 1e-8 ] # TODO : these are estimates, based on gps's 1Hz
 
-def dead_reckon_apply(x_hat, P_hat, step):
+def dead_reckon_apply(x_hat, P_hat, step, idx):
 
     # No-op if we didn't move
     if (step[1] == 0 and step[2] == 0):
@@ -131,14 +131,39 @@ def dead_reckon_apply(x_hat, P_hat, step):
     for k in range(1,len(dY)):
         cov = cov + W * np.array([dY[k]]).T @ np.array([dY[k]]) # exterior product
 
+    print("cov")
+    for row in cov:
+        print(",".join("{:12.8f}".format(v) for v in row))
+    print()
     x_tilde = T(z)                                       # transform the mean
     P_tilde = cov
 
+    for zz in Z:
+        euler = [-zz[2], 0, 0]
+        xyz = np.array([ t.apply_euler(euler, _xyz) for _xyz in [
+            [0,0.05,0],
+            [-0.01,0,0],
+            [0.01,0,0],
+            [0,0.05,0]
+        ]]) + np.array([zz[0],zz[1],0])
+        plt.fill(xyz[:,0],xyz[:,1],color="green", alpha=0.5)
+    for zz in Y:
+        euler = [-zz[2], 0, 0]
+        xyz = np.array([ t.apply_euler(euler, _xyz) for _xyz in [
+            [0,0.05,0],
+            [-0.01,0,0],
+            [0.01,0,0],
+            [0,0.05,0]
+        ]]) + np.array([zz[0],zz[1],0])
+        plt.fill(xyz[:,0],xyz[:,1],color="blue", alpha=0.5)
+    #if idx > 10:
+    #    plt.show()
+
     return x_tilde, P_tilde
 
-def dead_reckon(x_hat, P_hat, steps):
+def dead_reckon(x_hat, P_hat, steps, i):
     for step in steps:
-        x_hat, P_hat = dead_reckon_apply(x_hat, P_hat, step)
+        x_hat, P_hat = dead_reckon_apply(x_hat, P_hat, step, i)
     return x_hat, P_hat
 
 ## ======================
@@ -166,7 +191,7 @@ phs = [P_hat]
 
 for i in range(50):
     print("==================================== "+str(i+1))
-    x_hat, P_hat = dead_reckon(x_hat, P_hat, steps)
+    x_hat, P_hat = dead_reckon(x_hat, P_hat, steps, i)
     xhs.append(x_hat)
     phs.append(P_hat)
 

test8.py
@@ -2,6 +2,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import robotransforms.euclidean.euclidean_pure as t
+from scipy.spatial.transform import Rotation as R
+
+def plot_quaternions(quaternions, nrows=1):
+    n_quaternions = quaternions.shape[0]
+    ncols = int(np.ceil(n_quaternions / nrows))
+
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(3*ncols, 3*nrows))
+    axes = axes.flatten()
+
+    for i in range(n_quaternions):
+        quat = quaternions[i]
+        r = R.from_quat(quat)
+        yaw = r.as_euler('zyx')[2]
+
+        axes[i].arrow(0, 0, np.cos(yaw), np.sin(yaw), head_width=0.1, head_length=0.1, fc='k', ec='k')
+        axes[i].set_xlim(-1.2, 1.2)
+        axes[i].set_ylim(-1.2, 1.2)
+        axes[i].set_aspect('equal')
+        axes[i].set_title(f'Quaternion {i+1}')
+
+    for i in range(n_quaternions, nrows*ncols):
+        axes[i].axis('off')
+
+    plt.tight_layout()
+    plt.show()
 
 ## ======================
 ## CODE
@@ -40,14 +65,51 @@ def dead_reckon_step_errors( dl, dr, dl_scale=0.005): #vave, vdiff, dl_scale=0.0
     #  [ var_dl, cov_dldr, var_dr, var_b, var_c, var_d ]
     return [ ddl*ddl + 1e-8, rho*ddl*ddr, ddr*ddr + 1e-8 ] # TODO : these are estimates, based on gps's 1Hz
 
-def dead_reckon_apply(x_hat, P_hat, step):
+# rotvec is RV from
+# https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6339217/pdf/sensors-19-00149.pdfh
+def quat2rotvec(quat):
+    if np.allclose(quat, np.array([1., 0., 0., 0.])):
+        return np.array([0., 0., 0.])
+
+    rotvec = np.zeros(3)
+    qvec_norm = np.linalg.norm(quat[1:])
+    # TODO: Figure out what to do otherwise
+    print("quat:", quat)
+    if quat[0] >= 0:
+        rotvec = 2 * np.arcsin(qvec_norm) * quat[1:] / qvec_norm
+    else:
+        # Figure out what to do here...
+        print("GOT HERE quat2rotvec")
+        rotvec = None
+
+    return rotvec
+
+def rotvec2quat(rotvec):
+    if np.allclose(rotvec, np.zeros_like(rotvec)):
+        return np.array([1., 0., 0., 0.])
+
+    print("rotvec:", rotvec)
+    rotvec_norm = np.linalg.norm(rotvec)
+    print("rotvec norm:", rotvec_norm)
+    if rotvec_norm <= np.pi:
+        quat = np.zeros(4)
+        quat[0] = np.cos(rotvec_norm / 2)
+        quat[1:] = np.sin(rotvec_norm / 2) * rotvec / rotvec_norm
+    else:
+        # Figure out what to do here...
+        print("GOT HERE rotvec2quat")
+        quat = None
+
+    return quat
+
+def dead_reckon_apply(x_hat, P_hat, step, j):
 
     # No-op if we didn't move
     if (step[1] == 0 and step[2] == 0):
         return x_hat, P_hat
 
     # Build up extended state vector
-    SS = 3
+    SS = 4
     z = np.zeros((SS+2))
     z[:SS] = x_hat[:]
     z[SS+0] = step[1]
@@ -88,45 +150,57 @@ def dead_reckon_apply(x_hat, P_hat, step):
         # print(w.T)
         raise e
 
-    #                x_hat y_hat a  b  c  d
-    lrQ = t.lrq2lrQ([z[0], z[1], 0, 0 ,0, z[2]])
+    # NOTE: dz (the cholesky decomp) gives us vectors with euclidean
+    # dimensions, i.e. dz[2] is e[2], where e = [e0, e1, e2] are rotation
+    # vector embeddings of S^3 in R^3
+    print("dz")
+    for s in dz:
+        print(*[ "{: >9.5f}".format(_s) for _s in s])
+
+    #                x_hat y_hat z  a     b  c  d
+    lrQ = np.array([z[0], z[1],  0, z[2], 0 ,0, z[3]])
+    print("lrQ", lrQ)
     # Computing sigma points
     for i in range(L):
         # The first SS are gotten via composition
-        dlrQ = t.lrq2lrQ([dz[i][0], dz[i][1], 0, 0 ,0, dz[i][2]])
-        lrQ1 = t.compose_lrQ(lrQ, dlrQ)
-        #                         x        y        d
-        Z[1 + i][:SS] = np.array([lrQ1[0], lrQ1[1], lrQ1[6]])
-        dlrQ = t.lrq2lrQ([-dz[i][0], -dz[i][1], 0, 0 ,0, -dz[i][2]])
+        dlrQ_pos = np.array([dz[i][0], dz[i][1], 0])
+        dlrQ_rv = np.array([0, 0, dz[i][2]])
+        dlrQ = np.hstack([dlrQ_pos, rotvec2quat(dlrQ_rv)])
+        #dlrQ = np.array([dz[i][0], dz[i][1], 0, np.cos(dz[i][2]), 0 ,0, dz[i][2]])
+        lrQ1 = t.compose_lrQ(lrQ, dlrQ) 
+        #                         x        y        a        d
+        Z[1 + i][:SS] = np.array([lrQ1[0], lrQ1[1], lrQ1[3], lrQ1[6]])
+
+        dlrQ_pos = np.array([-dz[i][0], -dz[i][1], 0]) 
+        dlrQ_rv = np.array([0, 0, -dz[i][2]])
+        dlrQ = np.hstack([dlrQ_pos, rotvec2quat(dlrQ_rv)])
+        #dlrQ = np.array([-dz[i][0], -dz[i][1], 0, np.sqrt(1-dz[i][2]**2), 0, 0, -dz[i][2]])
         lrQ2 = t.compose_lrQ(lrQ, dlrQ)
-        Z[1 + i + L][:SS] = np.array([lrQ2[0], lrQ2[1], lrQ2[6]])
+        Z[1 + i + L][:SS] = np.array([lrQ2[0], lrQ2[1], lrQ2[3], lrQ2[6]])
 
         # The last 2 (dl, dr) are gotten via addition
         Z[1 + i][SS:] = z[SS:] + dz[i][SM:]
         Z[1 + i + L][SS:] = z[SS:] - dz[i][SM:]
     print("Z")
     for row in Z:
-        print(",".join("{:12.8f}".format(v) for v in row))
+        print(",".join("{:12.8f}".format(v) for v in row)) #, np.sqrt(row[2]**2+row[3]**2))
     print()
 
     # Transform to new vectors
     def T(_z):
-        #                      b  c  d
-        quat = t.redquat2quat([0 ,0, _z[2]])
+        #               a       b  c  d
+        quat = np.array([_z[2], 0 ,0, _z[3]])
         dx = dead_reckon_step( quat=quat, dl=z[SS+0], dr=z[SS+1]) #, vave=step[7], vdiff=step[8] )
         # Taking current quat and applying step quat rotation to it
-        next_quat = t.compose_quat(quat,step[3:7])
+        next_quat = t.compose_quat(quat, step[3:7])
         # Only return offset, this is marginalizing out all the quat dependances
         # return _z[:3] + dx
-        return np.hstack([_z[:2] + dx[:2], next_quat[3]])
+        return np.hstack([_z[:2] + dx[:2], next_quat[0], next_quat[3]])
 
     Y = np.array([ T(_z) for _z in Z ])
-    #plt.plot(Z[:,0],Z[:,1],"go")
-    #plt.plot(Y[:,0],Y[:,1],"bx")
-    #plt.show()
     print("Y")
     for row in Y:
-        print(",".join("{:12.8f}".format(v) for v in row))
+        print(",".join("{:12.8f}".format(v) for v in row))#, np.sqrt(row[2]**2+row[3]**2))
     print()
 
     M = len(Y[0])
@@ -139,9 +213,10 @@ def dead_reckon_apply(x_hat, P_hat, step):
 
     # Take a weighted average of the position components
     y_pos_bar = (Y_pos[0]*W0 + np.sum(Y_pos[1:]*W,axis=0))[:2]
-
-    # Iteratively calculate the quaternion mean
-    Y_quat = np.array([t.redquat2quat([0,0,d]) for d in Y[:,2]])
+    
+    ## Iteratively calculate the quaternion mean
+    Y_quat = np.array([[a,0,0,d] for a,d in zip(Y[:,2],Y[:,3])])
+    #print("Y_quat", Y_quat)
     q_bar = Y_quat[0]
     q_bar_inv = t.invert_quat(Y_quat[0])
     EPS = 1e-12
@@ -150,15 +225,18 @@ def dead_reckon_apply(x_hat, P_hat, step):
     error = 1
     while ( i < MAX and error > EPS ):
         i += 1
-        e_rquats = np.array([t.quat2redquat(t.compose_quat(q_bar_inv, y)) for y in Y_quat])
-        e = (e_rquats[0]*W0 + np.sum(e_rquats[1:]*W,axis=0))
+        e_rvecs = np.array([quat2rotvec(t.compose_quat(q_bar_inv, y)) for y in Y_quat])
+        e = (e_rvecs[0]*W0 + np.sum(e_rvecs[1:]*W,axis=0))
         error = np.linalg.norm(e)
-        q_bar = t.compose_quat(q_bar, t.redquat2quat(e))
+        q_bar = t.compose_quat(q_bar, rotvec2quat(e))
         q_bar_inv = t.invert_quat(q_bar)
 
     print(i, error)
 
-    dY = np.hstack([Y_pos-y_pos_bar, e_rquats[:,2:]])
+    dY = np.hstack([Y_pos-y_pos_bar, e_rvecs[:,2:]]) 
+
+    #dY = Y - y_bar  
+
     # for row in dY:
     #     print(",".join("{:12.8f}".format(v) for v in row))
     # print()
@@ -169,15 +247,42 @@ def dead_reckon_apply(x_hat, P_hat, step):
     cov = W0 * np.array([dY[0]]).T @ np.array([dY[0]]) # exterior product
     for k in range(1,len(dY)):
         cov = cov + W * np.array([dY[k]]).T @ np.array([dY[k]]) # exterior product
-
+ 
+    print("cov")
+    for row in cov:
+        print(",".join("{:12.8f}".format(v) for v in row))
+    print()
     x_tilde = T(z)                                       # transform the mean
     P_tilde = cov
 
+    for zz in Z:
+        quat = np.array([zz[2], 0,0, -zz[3]])
+        xyz = np.array([ t.apply_quat(quat, _xyz) for _xyz in [
+            [0,0.05,0],
+            [-0.01,0,0],
+            [0.01,0,0],
+            [0,0.05,0]
+        ]]) + np.array([zz[0],zz[1],0])
+        plt.fill(xyz[:,0],xyz[:,1],color="green", alpha=0.5)
+    for zz in Y:
+        quat = np.array([zz[2], 0,0, -zz[3]])
+        xyz = np.array([ t.apply_quat(quat, _xyz) for _xyz in [
+            [0,0.05,0],
+            [-0.01,0,0],
+            [0.01,0,0],
+            [0,0.05,0]
+        ]]) + np.array([zz[0],zz[1],0])
+        plt.fill(xyz[:,0],xyz[:,1],color="blue", alpha=0.5)
+
+    #if j > 10:
+    #    plt.show()
+    #    plot_quaternions(Y_quat, 3)
+
     return x_tilde, P_tilde
 
-def dead_reckon(x_hat, P_hat, steps):
-    for step in steps:
-        x_hat, P_hat = dead_reckon_apply(x_hat, P_hat, step)
+def dead_reckon(x_hat, P_hat, steps, j):
+    for i, step in enumerate(steps):
+        x_hat, P_hat = dead_reckon_apply(x_hat, P_hat, step, j)
     return x_hat, P_hat
 
 ## ======================
@@ -189,15 +294,16 @@ dl = 0.08
 dr = 0.07
 to2 = ( dl - dr ) / ( 2 * D )
 
-x_hat = np.array([0,0,0])
-P_hat = np.diag([1e-1,1e-1,0.05])
+#                 x,y,a,d
+x_hat = np.array([0,0,1,0])
+P_hat = np.diag([1e-1,1e-1,0.2])
 step = np.array([
     43994.76,          # ts
     dl, dr,      # dl, dr
     np.cos(to2), 0, 0, -np.sin(to2),    # Dq: a,b,c,d
     #0.215, 0.143,     # vave, vdiff
 ])
-steps = 3*[step]
+steps = 1*[step]
 
 
 xhs = [x_hat]
@@ -205,7 +311,7 @@ phs = [P_hat]
 
 for i in range(50):
     print("==================================== "+str(i+1))
-    x_hat, P_hat = dead_reckon(x_hat, P_hat, steps)
+    x_hat, P_hat = dead_reckon(x_hat, P_hat, steps, i)
     xhs.append(x_hat)
     phs.append(P_hat)
 